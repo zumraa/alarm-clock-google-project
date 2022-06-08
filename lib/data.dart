603b57047a7a7a9a72c3ab7f3a43911e0fb54c29
zumraa <104394182+zumraa@users.noreply.github.com>
2022-06-08 21:24:07 +0300

    upload

---
+++ lib/data.dart
@@ -0,0 +1,22 @@
+import 'package:clock_app/enums.dart';
+
+import 'models/alarm_info.dart';
+import 'models/menu_info.dart';
+
+List<MenuInfo> menuItems = [
+  MenuInfo(MenuType.clock,
+      title: 'Clock', imageSource: 'assets/clock_icon.png'),
+  MenuInfo(MenuType.alarm,
+      title: 'Alarm', imageSource: 'assets/alarm_icon.png'),
+];
+
+List<AlarmInfo> alarms = [
+  AlarmInfo(
+      alarmDateTime: DateTime.now().add(Duration(hours: 1)),
+      title: 'Office',
+      gradientColorIndex: 0),
+  AlarmInfo(
+      alarmDateTime: DateTime.now().add(Duration(hours: 2)),
+      title: 'Sport',
+      gradientColorIndex: 1),
+];
